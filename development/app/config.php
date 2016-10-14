8339ac7a78d9add0247893d869af0a0a8f6b188f
keanu1997 <music-dude@hotmail.nl>
2016-10-14 10:18:19 +0200

    delete old all old copypaste spagetti code

--- development/app/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// base url
-define('BASE_URL', 'http://localhost/barroc-it-groep-5/development/');
-
-// app path name
-define('APP_PATH', realpath(__DIR__) . '/');
-// public app name
-define('PUBLIC_PATH', realpath(__DIR__ . '../public/'));
