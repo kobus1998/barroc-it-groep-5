2d5837ca3a44c15aecb9437b5c3bba09bbc6a94f
kobus1998 <kobus.haarhuis@hotmail.com>
2016-10-18 15:39:28 +0200

    register bootstrap

--- development/public/html/register.php
+++ development/public/html/register.php
@@ -4,28 +4,18 @@
 </head>
 
 <body>
-    <div class="header">
     <?php
-    // if session role = admin
-    // require PUBLIC_PATH . 'html/parts/header_admin.php';
-    // else if session role = sales
-    // require PUBLIC_PATH . 'html/parts/header_sales.php';
-
-    //etc.
-
-
+    require 'parts/header_admin.php';
     ?>
-    </div>
     <div class="main-content">
         <div class="container">
-            <h1>Register</h1>
-            <form method="post" action="<?php echo BASE_URL ?>/development/app/controller/authController.php">
-                <p>* is required</p>
+            <h1 class="text-center">Register</h1>
+            <form class="col-md-6 col-md-offset-3" method="post" action="<?php echo BASE_URL ?>/development/app/controller/authController.php">
                 <div class="form-group">
-                    * <label for="username">
+                    <label for="username">
                         Department
                     </label>
-                    <select  name="username">
+                    <select  name="username" class="form-control">
                         <option></option>
                         <option value="sales">Sales</option>
                         <option value="finance">Finance</option>
@@ -34,19 +24,19 @@
                     </select>
                 </div>
                 <div class="form-group">
-                    * <label for="password">
+                    <label for="password">
                         Password
                     </label>
-                    <input type="password" name="password">
+                    <input type="password" name="password" class="form-control">
                 </div>
                 <div class="form-group">
-                    * <label for="password-repeat">
+                    <label for="password-repeat">
                         Repeat Password
                     </label>
-                    <input type="password" name="password-repeat">
+                    <input type="password" name="password-repeat" class="form-control">
                 </div>
 
-                <input type="submit" name="type" value="register">
+                <input class="btn-primary" type="submit" name="type" value="register">
                 
             </form>
         </div>
