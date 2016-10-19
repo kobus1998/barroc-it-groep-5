a9a9cf9f8cc52114b23d4f4736a985f2d96ba681
timschachtschabel <timschachtschabel14@gmail.com>
2016-10-19 11:25:25 +0200

    dit kwam niet door for some reason

--- development/public/html/admin.php
+++ development/public/html/admin.php
@@ -1,7 +1,8 @@
 <?php
 require realpath(__dir__ . '/parts/header.php');
 
-if(isset($_SESSION['username']) != 'Admin'){
+// Only let people in with the username of the given department
+if(isset($_SESSION['username']) && ($_SESSION['username']) != 'Admin'){
     $user->redirectMessage('index.php', 'Not logged in');
 }
 ?>
