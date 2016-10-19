f1e26588ed3d7839b29706b3e487f13d463870ac
kobus1998 <kobus.haarhuis@hotmail.com>
2016-10-19 09:56:26 +0200

    comments verbeterd

--- development/app/controller/authController.php
+++ development/app/controller/authController.php
@@ -35,10 +35,15 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
             $user->redirectMessage("index.php", $message);
             die;
         }
-        // first letter capitalised
+        /*
+         * first letter capitalised
+         */
+
         $username = ucfirst($username);
-        // database connection
-        // Select everything from username
+        /*
+         * database connection
+         * Select everything from username
+         */
         $db = Database::getInstance();
         $sql = "SELECT * FROM `tbl_users` WHERE :username = `username`";
         $stmt = $db->pdo->prepare($sql);
@@ -59,18 +64,24 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
 
         $data = $stmt->fetch(PDO::FETCH_OBJ);
         $hashedPassword = $data->password;
-        // verify the hashed password
+        /*
+         * verify the hashed password
+         */
         if (! password_verify($password, $hashedPassword) ) {
             $message = 'wrong password';
             $user->redirectMessage('index.php', $message);
             die;
         }
-        // Information into array
+        /*
+         *  Information into array
+         */
         $info = [
             'user_id'   => $data->user_id,
             'username'  => $data->username
         ];
-        // use function login
+        /*
+         *use function login
+         */
         $user->login($info);
     }
     /*
@@ -81,30 +92,42 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST')
         $username = $_POST['username'];
         $password = $_POST['password'];
         $passwordRepeat = $_POST['password-repeat'];
-        // if any of the fields are empty
+        /*
+         *  if any of the fields are empty
+         */
         if(empty($username) || empty($password) || empty($passwordRepeat)) 
         {
             $message = 'Some fields are empty';
             $user->redirectMessage("register.php", $message);
             die;
-            // if password is less then 5 chars
+            /*
+             *  if password is less then 5 chars
+             */
         } else if(strlen($password) < 5)
         {
             $message = "password too short";
             $user->redirectMessage('register.php', $message);
             die;
-            // if password is not the same as password-repeat
+            /*
+             *  if password is not the same as password-repeat
+             */
         } else if( $password != $passwordRepeat) 
         {
             $message = 'Password does not match with repeat password';
             $user->redirectMessage("register.php", $message);
             die;
         }
-        // capitalise the first letter
+        /*
+         *  capitalise the first letter
+         */
         $username = ucfirst($username);
-        // hash the password
+        /*
+         *  hash the password
+         */
         $password = password_hash($password, PASSWORD_DEFAULT);
-        // use function register
+        /*
+         *  use function register
+         */
         $user->register($username, $password);
         $message = 'success';
         $user->redirectMessage('index.php', $message);
