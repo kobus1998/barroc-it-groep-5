d46d2e947200cb428db063e4c61c73747ceb0936
kobus1998 <kobus.haarhuis@hotmail.com>
2016-10-19 11:11:34 +0200

    customer-list-validation-roles

--- development/public/html/customer_list.php
+++ development/public/html/customer_list.php
@@ -1,1 +1,43 @@
 <?php
+$db = Database::getInstance();
+
+$customers = $db->pdo->
+             query("SELECT * FROM `tbl_customers`")
+             ->fetchAll(PDO::FETCH_ASSOC);
+
+if($user->username == 'Sales') {
+
+    ?>
+    <!--html-->
+
+
+    <?php
+}
+
+if($user->username == 'Finance') {
+    ?>
+    <!--html-->
+
+    <?php
+
+    ?>
+
+
+    <?php
+}
+
+if($user->username == 'Admin') {
+    ?>
+    <!--html-->
+
+
+    <?php
+}
+
+if($user->username == 'Development') {
+    ?>
+    <!--html-->
+
+
+    <?php
+}
