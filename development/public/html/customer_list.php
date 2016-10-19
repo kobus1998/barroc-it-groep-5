3a402b2c0dc716d6a79a939b68b9b8d0ac28e1d2
kobus1998 <kobus.haarhuis@hotmail.com>
2016-10-19 12:00:02 +0200

    customer list th not hard coded

--- development/public/html/customer_list.php
+++ development/public/html/customer_list.php
@@ -5,6 +5,8 @@ $customers = $db->pdo->
              query("SELECT * FROM `tbl_customers`")
              ->fetchAll(PDO::FETCH_ASSOC);
 
+$tableHeader = $db->pdo->query("SHOW COLUMNS FROM `tbl_customers`")->fetchAll(PDO::FETCH_ASSOC);
+
 if($user->username == 'Sales') {
 
     ?>
@@ -12,7 +14,13 @@ if($user->username == 'Sales') {
             <div class="container">
                 <table class="table table-striped">
                     <tr class="row">
-                    <th>Company name</th>
+                        <?php
+                        foreach ($tableHeader as $item)
+                        {
+                            echo '<th>' . $item['Field'] . '</th>';
+                        }
+                        ?>
+                    <!--<th>Company name</th>
                     <th>Adress 1</th>
                     <th>Housenumber 1</th>
                     <th>Zip code 1</th>
@@ -30,7 +38,7 @@ if($user->username == 'Sales') {
                     <th>Offer status</th>
                     <th>Potential customer y/n</th>
                     <th>Appointment day</th>
-                    <th>Next action</th>
+                    <th>Next action</th>-->
                     </tr>
                 </table>
             </div>
@@ -43,7 +51,39 @@ if($user->username == 'Sales') {
 if($user->username == 'Finance') {
     ?>
     <!--html-->
-
+    <section class="customers">
+        <div class="container">
+            <table class="table table-striped">
+                <tr class="row">
+                    <?php
+                    foreach ($tableHeader as $item)
+                    {
+                        echo '<th>' . $item['Field'] . '</th>';
+                    }
+                    ?>
+                    <!--<th>Company name</th>
+                    <th>Adress 1</th>
+                    <th>Housenumber 1</th>
+                    <th>Zip code 1</th>
+                    <th>City 1</th>
+                    <th>Housenumber 2</th>
+                    <th>Zipcode 2</th>
+                    <th>City 2</th>
+                    <th>Contact person</th>
+                    <th>Initials</th>
+                    <th>Phone number 1</th>
+                    <th>Phone number 2</th>
+                    <th>Fax</th>
+                    <th>Email</th>
+                    <th>Invoicenumber</th>
+                    <th>Offer status</th>
+                    <th>Potential customer y/n</th>
+                    <th>Appointment day</th>
+                    <th>Next action</th>-->
+                </tr>
+            </table>
+        </div>
+    </section>
     <?php
 
     ?>
