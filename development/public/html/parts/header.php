8ee5bcc73c8c67ebb4064a268bc5448eac369798
kobus1998 <kobus.haarhuis@hotmail.com>
2016-10-31 13:35:12 +0100

    tinymce
    textfield dingetje, is best vet

--- development/public/html/parts/header.php
+++ development/public/html/parts/header.php
@@ -18,4 +18,6 @@
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
                 integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!--<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+        <script>tinymce.init({ selector:'textarea' });</script>-->
     </head>
