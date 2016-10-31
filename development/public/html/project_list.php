25195f6cac225196e4b84bc8451901da108e951f
timschachtschabel <timschachtschabel14@gmail.com>
2016-10-31 10:59:10 +0100

    nog een foutje verbeterd + project list begin

--- development/public/html/project_list.php
+++ development/public/html/project_list.php
@@ -6,34 +6,34 @@ $user->checkPage($user->username);
 
 if($user->username == 'Sales') {
     include realpath(__DIR__. '/parts/header_sales.php');
-    $invoices = $db->pdo->
-    query("SELECT * FROM `tbl_invoices`")
+    $projects = $db->pdo->
+    query("SELECT * FROM `tbl_projects`")
         ->fetchAll(PDO::FETCH_ASSOC);
     ?>
-    <section class="invoices">
+    <section class="projects">
         <div class="container">
             <table class="table ">
                 <thead>
                 <tr>
                     <th>#</th>
-                    <th>Invoice number</th>
-                    <th>Project id</th>
-                    <th>Paid</th>
-                    <th>Invoice info</th>
+                    <th>Customer id</th>
+                    <th>Project name</th>
+                    <th>Deadline</th>
+                    <th>Project info</th>
 
                 </tr>
                 </thead>
                 <tbody>
 
                 <?php
-                foreach ($invoices as $item)
+                foreach ($projects as $item)
                 {
                     echo '<tr>';
-                    echo '<td>' . $item['invoice_id'] . '</td>';
-                    echo '<td>' . $item['invoice_nr'] . '</td>';
                     echo '<td>' . $item['project_id'] . '</td>';
-                    echo '<td>' . $item['paid'] . '</td>';
-                    echo '<td><a href=customer/edit_customer.php?customerid="' . $item["invoice_id"] .'">Invoice info</a></td>';
+                    echo '<td>' . $item['customer_id'] . '</td>';
+                    echo '<td>' . $item['Project_name'] . '</td>';
+                    echo '<td>' . $item['deadline'] . '</td>';
+                    echo '<td><a href=customer/edit_customer.php?customerid="' . $item["project_id"] .'">Project info</a></td>';
                     echo '</tr>';
                 }
                 ?>
@@ -49,8 +49,8 @@ if($user->username == 'Sales') {
 
 if($user->username == 'Finance') {
     include realpath(__DIR__. '/parts/header_finance.php');
-    $invoices = $db->pdo->
-    query("SELECT * FROM `tbl_invoices`")
+    $projects = $db->pdo->
+    query("SELECT * FROM `tbl_project`")
         ->fetchAll(PDO::FETCH_ASSOC);
     ?>
     <section class="invoices">
@@ -131,6 +131,53 @@ if($user->username == 'Admin') {
             </table>
         </div>
     </section>
+
+
+
+    <?php
+}
+
+if($user->username == 'Development') {
+    include realpath(__DIR__. '/parts/header_development.php');
+    $invoices = $db->pdo->
+    query("SELECT * 
+           from `tbl_invoices` 
+            ")
+        ->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <section class="invoices">
+        <div class="container">
+            <table class="table ">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Invoice number</th>
+                    <th>Project id</th>
+                    <th>Paid</th>
+                    <th>Invoice info</th>
+
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php
+                foreach ($invoices as $item)
+                {
+                    echo '<tr>';
+                    echo '<td>' . $item['invoice_id'] . '</td>';
+                    echo '<td>' . $item['invoice_nr'] . '</td>';
+                    echo '<td>' . $item['project_id'] . '</td>';
+                    echo '<td>' . $item['paid'] . '</td>';
+                    echo '<td><a href=customer/edit_customer.php?customerid="' . $item["invoice_id"] .'">Invoice info</a></td>';
+                    echo '</tr>';
+                }
+                ?>
+
+                </tbody>
+            </table>
+        </div>
+    </section>
+
 
     <?php
 }
