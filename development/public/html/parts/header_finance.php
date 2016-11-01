10831e3574a0ada298ec35c3b98eddc0595775f1
keanu1997 <music-dude@hotmail.nl>
2016-11-01 10:12:36 +0100

    switched order in menu for finance

--- development/public/html/parts/header_finance.php
+++ development/public/html/parts/header_finance.php
@@ -7,19 +7,20 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customer<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="<?php echo BASE_URL ?>/development/public/html/customer_list.php">Customer list</a></li>
-                    </ul>
-                </li>
-
-                <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Invoice<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="<?php echo BASE_URL;?>/development/public/html/invoice_list.php">Invoices list</a></li>
                         <li><a href="#">Add invoice</a></li>
                     </ul>
                 </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customer<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="<?php echo BASE_URL ?>/development/public/html/customer_list.php">Customer list</a></li>
+                    </ul>
+                </li>
+
+
             </ul>
             <ul class="nav navbar-right">
                 <li><a href="<?php echo BASE_URL;?>/development/app/controller/authController.php?type=logout"><button class="btn btn-warning">Logout</button></a></li>
