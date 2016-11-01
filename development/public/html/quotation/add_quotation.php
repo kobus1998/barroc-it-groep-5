c001b2e91aeba058e439d1cbf2abdd5ff311688f
kobus1998 <kobus.haarhuis@hotmail.com>
2016-11-01 09:53:15 +0100

    add quotation beginnetje hahah

--- development/public/html/quotation/add_quotation.php
+++ development/public/html/quotation/add_quotation.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kobus
- * Date: 11/1/2016
- * Time: 9:35 AM
- */
+require_once realpath(__DIR__ . '/../parts/header.php');
+
+$user->checkPage('Sales');
+?>
+<title>Add quotation</title>
+</head>
+
+<div class="header">
+    <?php require realpath(__DIR__ . '/../parts/header_sales.php'); ?>
+</div>
+
+<div class="main-content">
+    <div class="container">
+        <h1 class="col-md-6 col-md-offset-3">Add quotation</h1>
+        <form action="" method="post">
+            <div class="form-group">
+                <label>
+                    Customer
+                    <input type="button">
+                </label>
+            </div>
+        </form>
+    </div>
+</div>
