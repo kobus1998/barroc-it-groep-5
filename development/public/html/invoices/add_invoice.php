9e0b2546d255b82ef2e197bb11f9bc4ae932679f
keanu1997 <music-dude@hotmail.nl>
2016-11-01 11:34:52 +0100

    debug header in add_invoice.php

--- development/public/html/invoices/add_invoice.php
+++ development/public/html/invoices/add_invoice.php
@@ -13,7 +13,7 @@ $user->checkPage('Finance');
 <body>
 <div class="header">
 	<?php
-	include '../parts/header_sales.php';
+	include '../parts/header_finance.php';
 	?>
 </div>
 <div class="main-content">
