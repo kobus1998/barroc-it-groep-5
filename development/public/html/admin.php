32fe2450cfecc9aac9679a610305df2991a38f56
keanu1997 <music-dude@hotmail.nl>
2016-11-02 08:50:43 +0100

    set up some first page when u logged in as admin

--- development/public/html/admin.php
+++ development/public/html/admin.php
@@ -3,18 +3,5 @@ require realpath(__dir__ . '/parts/header.php');
 
 // Only let people in with the username of the given department
 $user->checkPage('Admin');
+$user->redirectMessage('register.php', $message);
 ?>
-
-<body>
-
-    <div class="header">
-    <?php
-    include 'parts/header_admin.php';
-    ?>
-    </div>
-
-    <div class="main-content">
-
-    </div>
-</body>
-</html>
--- development/public/html/parts/header_admin.php
+++ development/public/html/parts/header_admin.php
@@ -7,6 +7,9 @@
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
+                    <li><a href="<?php echo BASE_URL ?>/development/public/html/register.php">Register</a></li>
+                </ul>
+                <ul class="nav navbar-nav">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customers<span class="caret"></span></a>
                         <ul class="dropdown-menu">
@@ -38,10 +41,6 @@
                             <li><a href="#">Add project</a></li>
                         </ul>
                     </li>
-
-                    <ul class="nav navbar-nav">
-                        <li><a href="<?php echo BASE_URL ?>/development/public/html/register.php">Register</a></li>
-                    </ul>
                 </ul>
                 <ul class="nav navbar-right">
                     <li><a href="<?php echo BASE_URL;?>/development/app/controller/authController.php?type=logout"><button class="btn btn-warning">Logout</button></a></li>
