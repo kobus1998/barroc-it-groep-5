2ab408e5bff0af5ed8c257297900bc9de40cbd32
keanu1997 <music-dude@hotmail.nl>
2016-11-02 11:06:25 +0100

    fix header bug in add_invoice

--- development/public/html/invoices/add_invoice.php
+++ development/public/html/invoices/add_invoice.php
@@ -13,7 +13,14 @@ $user->checkPage('Finance');
 <body>
 <div class="header">
 	<?php
-	include '../parts/header_finance.php';
+	if($user->username == "Sales") {
+		require "../parts/header_sales.php";
+	} elseif($user->username == "Finance") {
+		require "../parts/header_finance.php";
+	} elseif($user->username == "Admin") {
+		require "../parts/header_admin.php";
+	}
+
 	?>
 </div>
 <div class="main-content">
