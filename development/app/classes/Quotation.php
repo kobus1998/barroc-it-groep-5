dd0fa6c91f7c8c2b2f67f3edbc2a8b566082849e
timschachtschabel <timschachtschabel14@gmail.com>
2016-11-02 11:01:16 +0100

    add quotation(fuck off kyanu)

--- development/app/classes/Quotation.php
+++ development/app/classes/Quotation.php
@@ -27,5 +27,22 @@ class Quotations {
         return false;
     }
 
+    public function addQuotation($quotation) {
+
+        $db = Database::getInstance();
+
+        $sql = "INSERT INTO `tbl_quotations` (quotation_number, quotation_date, order_type) 
+                                             VALUES (:quotation_number, :quotation_date, :order_type)";
+
+        $stmt = $db->pdo->prepare($sql);
+        $stmt->bindParam(':quotation_number', $quotation['quotation_number']);
+        $stmt->bindParam(':quotation_date', $quotation['quotation_date']);
+        $stmt->bindParam(':order_type', $quotation['order_type']);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+    }
 
 }
