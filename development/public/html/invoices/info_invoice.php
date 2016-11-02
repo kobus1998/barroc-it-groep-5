f4e0bfcb36f88e29520242345791fabf3d95a308
kobus1998 <kobus.haarhuis@hotmail.com>
2016-11-02 09:54:23 +0100

    info invoice

--- development/public/html/invoices/info_invoice.php
+++ development/public/html/invoices/info_invoice.php
@@ -8,6 +8,8 @@ $invoiceId = $_GET['invoiceid'];
 $stmt = $db->pdo->query("SELECT * FROM `tbl_invoices`
                  WHERE `tbl_invoices`.invoice_id = " . $invoiceId);
 $invoiceDB = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$projectId = $invoiceDB[0]['project_id']
 ?>
 <title>Invoice Detail</title>
 </head>
@@ -24,6 +26,38 @@ $invoiceDB = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
         
         ?>
+    </div>
+    <div class="main">
+        <div class="container">
+            <div class="container">
+                <?php if($user->username == 'Admin' || $user->username == 'Finance'){ ?>
+                    <div class="container">
+                        <?php echo "<a href='edit_invoice.php?invoiceid=$invoiceId'>Edit this invoice</a>" ?><br>
+                        <?php echo "<a href='../project/info_project.php?projectid=$projectId'>Invoice from project</a>" ?>
+                    </div>
+                <?php } ?>
+            <h1 style="text-align:center;font-size:6rem">Invoice details</h1>
+            <div class="col-md-6 row">
+                <ul class="pull-right" style="list-style: none; font-weight: bold;">
+                    <li>Invoice nr</li>
+                    <li>Price</li>
+                    <li>Tax</li>
+                    <li>Total</li>
+                    <li>Paid</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <ul class="" style="list-style: none">
+                    <li><?php echo $invoiceDB[0]['invoice_nr'] ?></li>
+                    <li>€ <?php echo $invoiceDB[0]['price'] ?></li>
+                    <li>€ <?php echo $invoiceDB[0]['tax'] ?></li>
+                    <li>€ <?php echo $invoiceDB[0]['total'] ?></li>
+                    <li><?php if( $invoiceDB[0]['paid'] == 1 ){
+                            echo 'Yes';
+                        } else {echo 'No';} ?></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </body>
 
