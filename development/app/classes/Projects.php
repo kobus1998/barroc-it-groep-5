e74b82f37a2e824058eb994a7da12c956d721c8f
Amphyros <iebelneskediebel@gmail.com>
2016-11-03 11:05:48 +0100

    En nog meer edit project :exclamation:

--- development/app/classes/Projects.php
+++ development/app/classes/Projects.php
@@ -11,20 +11,17 @@ class Projects {
         $this->db = Database::getInstance();
     }
 
-    public function getProjects()
+    public function getProjects($project_id)
     {
-        if (1 == 1) {
-            $sql = "SELECT *
-                FROM tbl_projects
-                ORDER BY project_id DESC
-                LIMIT 30 ";
-        }
+        $sql = "SELECT * FROM tbl_projects
+                LEFT JOIN tbl_customers
+                  on tbl_customers.customer_id = tbl_projects.customer_id
+                WHERE project_id = :project_id";
 
         $stmt = $this->db->pdo->prepare($sql);
-        if ($stmt->execute()){
-            return true;
-        }
-        return false;
+        $stmt->bindParam(':project_id', $project_id);
+        $stmt->execute();
+        $GLOBALS['projectData'] = $stmt->fetchAll();
     }
 
     public function addProject($project, $date)
@@ -44,6 +41,52 @@ class Projects {
         $stmt->bindParam(':description', $project['description']);
 
         if($stmt->execute()) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public function editProject($project)
+    {
+
+        $db = Database::getInstance();
+
+        $sql = "UPDATE `tbl_projects`
+                SET `project_name` = :project_name,
+                    `hardwaresoftware` = :hardwaresoftware,
+                    `maintenance_contract` = :maintenance_contract,
+                    `description` = :description
+                WHERE `tbl_projects`.`project_id` = :id";
+
+        $stmt = $db->pdo->prepare($sql);
+        $stmt->bindParam(':project_name', $project['project_name']);
+        $stmt->bindParam(':hardwaresoftware', $project['hardwaresoftware']);
+        $stmt->bindParam(':maintenance_contract', $project['maintenance_contract']);
+        $stmt->bindParam(':description', $project['description']);
+        $stmt->bindParam(':id', $project['project_id']);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public function editProjectDate($project, $date)
+    {
+
+        $db = Database::getInstance();
+
+        $sql = "UPDATE `tbl_projects`
+                SET `deadline` = :deadline
+                WHERE `project_id` = :id";
+            
+        $stmt = $db->pdo->prepare($sql);
+        $stmt->bindParam(':deadline', $date);
+        $stmt->bindParam(':id', $project['project_id']);
+        
+        if ($stmt->execute()) {
             return true;
         }
         return false;
