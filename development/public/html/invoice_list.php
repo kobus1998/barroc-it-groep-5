ca106fa7ac29f8132779ef63967e02ccbb0cf3da
kobus1998 <kobus.haarhuis@hotmail.com>
2016-11-08 09:45:12 +0100

    search invoice list xd

--- development/public/html/invoice_list.php
+++ development/public/html/invoice_list.php
@@ -4,104 +4,6 @@ $db = Database::getInstance();
 
 $user->checkPage($user->username);
 
-if($user->username == 'Sales') {
-    include realpath(__DIR__. '/parts/header_sales.php');
-    $invoices = $db->pdo->
-    query("SELECT * FROM `tbl_invoices`
-          LEFT JOIN `tbl_projects`
-            ON `tbl_projects`.project_id = `tbl_invoices`.project_id
-          LEFT JOIN `tbl_customers`
-            on `tbl_customers`.customer_id = `tbl_projects`.customer_id          
-          ")
-        ->fetchAll(PDO::FETCH_ASSOC);
-
-    if(isset($_GET['search-invoice-list'])) {
-        $searchGET = $_GET['search-invoice-list'];
-
-        $stmt = $db->pdo->query("SELECT * FROM `tbl_invoices` WHERE `tbl_invoices`.invoice_nr like '%:searchGET%'");
-        $stmt->bindParam(':searchGET', $searchGET);
-        $searchSales =  $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    ?>
-    <section class="invoices">
-        <div class="container">
-            <?php if(!isset($_GET['messageDanger']) || $_GET['messageDanger'] == '') {
-            } else { ?>
-                <P class="alert-danger pull-right" style="padding: 7px!important;">
-                    <?= $_GET['messageDanger'] ?>
-                </P>
-            <?php }
-
-            if(!isset($_GET['messagePrimary']) || $_GET['messagePrimary'] == '') {
-            } else { ?>
-                <P class="alert-primary pull-right" style="padding: 7px!important;">
-                    <?= $_GET['messagePrimary'] ?>
-                </p>
-            <?php }
-
-            if(!isset($_GET['messageSuccess']) || $_GET['messageSuccess'] == '') {
-            } else { ?>
-                <P class="alert-success pull-right" style="padding: 7px!important;">
-                    <?= $_GET['messageSuccess'] ?>
-                </P>
-            <?php } ?>
-            <h2 class="text-center">Invoice list</h2>
-            <div class="search-invoice col-md-5 col-md-offset-8">
-                <form method="get" action="">
-                    <div class="form-group row">
-                        <label>Search invoice number
-                            <input type="search" name="search-invoice-list">
-                        </label>
-                        <button class="btn btn-warning glyphicon glyphicon-search" type="submit" name="type" value="search"></button>
-                    </div>
-                </form>
-            </div>
-            <table class="table ">
-                <thead>
-                <tr>
-                    <th>Invoice number</th>
-                    <th>Project id</th>
-                    <th>Paid</th>
-                    <th>Invoice info</th>
-
-                </tr>
-                </thead>
-                <tbody>
-
-                <?php
-                if(!isset($_GET['type'])) {
-                    foreach ($invoices as $item) {
-                        echo '<tr>';
-                        echo '<td>' . $item['invoice_nr'] . '</td>';
-                        echo '<td>' . $item['project_id'] . '</td>';
-                        echo '<td>' . $item['paid'] . '</td>';
-                        echo '<td><a href="invoices/info_invoice.php?invoiceid=' . $item["invoice_id"] . '" class="btn btn-info glyphicon glyphicon-eye-open"></a></td>';
-                        echo '</tr>';
-                    }
-                }
-
-                if(isset($_GET['type']) && $_GET['type'] == 'search') {
-                    foreach ($searchSales as $item)
-                    {
-                        echo '<tr>';
-                        echo '<td>' . $item['invoice_nr'] . '</td>';
-                        echo '<td>' . $item['project_id'] . '</td>';
-                        echo '<td>' . $item['paid'] . '</td>';
-                        echo '<td><a href="invoices/info_invoice.php?invoiceid=' . $item["invoice_id"] . '" class="btn btn-info glyphicon glyphicon-eye-open"></a></td>';
-                        echo '</tr>';
-                    }
-                }
-                ?>
-
-                </tbody>
-            </table>
-        </div>
-    </section>
-
-
-    <?php
-}
-
 if($user->username == 'Finance') {
     include realpath(__DIR__. '/parts/header_finance.php');
     $invoices = $db->pdo->
@@ -109,7 +11,8 @@ if($user->username == 'Finance') {
           LEFT JOIN `tbl_projects`
             ON `tbl_projects`.project_id = `tbl_invoices`.project_id
           LEFT JOIN `tbl_customers`
-            on `tbl_customers`.customer_id = `tbl_projects`.customer_id          
+            on `tbl_customers`.customer_id = `tbl_projects`.customer_id
+          WHERE tbl_invoices.paid = 0
           ")
         ->fetchAll(PDO::FETCH_ASSOC);
 
@@ -118,7 +21,9 @@ if($user->username == 'Finance') {
         $searchQuery = preg_replace("#[^0-9a-z]#i","",$searchGET);
         $searchQuery = '%'.$searchQuery.'%';
 
-        $searchSQL = "SELECT * FROM `tbl_invoices` WHERE tbl_invoices.invoice_nr LIKE :search_query";
+        $searchSQL = "SELECT * FROM `tbl_invoices` 
+                      WHERE tbl_invoices.invoice_nr LIKE :search_query 
+                      AND tbl_invoices.paid = 0";
 
         $stmt = $db->pdo->prepare($searchSQL);
         $stmt->bindParam(':search_query', $searchQuery);
@@ -158,7 +63,7 @@ if($user->username == 'Finance') {
                 <form method="get" action="">
                     <div class="form-group row">
                         <label>Search invoice number
-                            <input type="search" name="search-invoice-list" value="<?= $searchGET ?>">
+                            <input type="search" name="search-invoice-list" value="<?php if(isset($_GET['search-invoice-list'])) {echo $searchGET; } ?>">
                         </label>
                         <button class="btn btn-warning glyphicon glyphicon-search" type="submit" name="type" value="search"></button>
                     </div>
@@ -219,7 +124,9 @@ if($user->username == 'Admin') {
           LEFT JOIN `tbl_projects`
             ON `tbl_projects`.project_id = `tbl_invoices`.project_id
           LEFT JOIN `tbl_customers`
-            on `tbl_customers`.customer_id = `tbl_projects`.customer_id          
+            on `tbl_customers`.customer_id = `tbl_projects`.customer_id
+                      
+          WHERE tbl_invoices.paid = 0
           ")
         ->fetchAll(PDO::FETCH_ASSOC);
 
@@ -228,7 +135,9 @@ if($user->username == 'Admin') {
         $searchQuery = preg_replace("#[^0-9a-z]#i","",$searchGET);
         $searchQuery = '%'.$searchQuery.'%';
 
-        $searchSQL = "SELECT * FROM `tbl_invoices` WHERE tbl_invoices.invoice_nr LIKE :search_query";
+        $searchSQL = "SELECT * FROM `tbl_invoices` 
+                      WHERE tbl_invoices.invoice_nr LIKE :search_query 
+                      AND tbl_invoices.paid = 0";
 
         $stmt = $db->pdo->prepare($searchSQL);
         $stmt->bindParam(':search_query', $searchQuery);
@@ -264,7 +173,7 @@ if($user->username == 'Admin') {
                 <form method="get" action="">
                     <div class="form-group row">
                         <label>Search invoice number
-                            <input type="search" name="search-invoice-list" value="<?= $searchGET ?>">
+                            <input type="search" name="search-invoice-list" value="<?php if(isset($_GET['search-invoice-list'])) {echo $searchGET; } ?>">
                         </label>
                         <button class="btn btn-warning glyphicon glyphicon-search" type="submit" name="type" value="search"></button>
                     </div>
@@ -297,7 +206,7 @@ if($user->username == 'Admin') {
                 }
 
                 if(isset($_GET['type']) && $_GET['type'] == 'search' && $_GET['search-invoice-list'] != '') {
-                    foreach ($searchAdmin as $item)
+                    foreach ($searchResults as $item)
                     {
                         echo '<tr>';
                         echo '<td>' . $item['invoice_nr'] . '</td>';
