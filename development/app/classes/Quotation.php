e5f522acc5c2fac80c38921a9da8c73275d7ca8c
timschachtschabel <timschachtschabel14@gmail.com>
2016-11-10 11:58:16 +0100

    werkt nu?

--- development/app/classes/Quotation.php
+++ development/app/classes/Quotation.php
@@ -34,17 +34,12 @@ class Quotation {
         $sql = "INSERT INTO `tbl_quotations` (customer_id, quotation_number, quotation_date, order_type, description) 
                                       VALUES (:id, :quotation_number, :quotation_date, :order_type, :description)";
 
-        $sqlMail = "SELECT * FROM `tbl_customers`
-                    WHERE `:customerId` = `customer_id`";
-
         $stmt = $db->pdo->prepare($sql);
         $stmt->bindParam(':id', $quotation['customer_id']);
         $stmt->bindParam(':quotation_number', $quotation['quotation_number']);
         $stmt->bindParam(':quotation_date', $quotation['quotation_date']);
         $stmt->bindParam(':order_type', $quotation['order_type']);
         $stmt->bindParam(':description', $quotation['description']);
-        $stmtMail = $db->pdo->prepare($sqlMail);
-        $stmt->bindParam(':customerId' , $quotation['customer_id']);
 
         if($stmt->execute()) {
             return true;
